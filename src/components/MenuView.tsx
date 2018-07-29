adf2f944af0ff1e2a340280047460347a8ec7f8c
TT <codertina@gmail.com>
2018-07-29 16:04:46 +0530

    Tina | Refactoring

--- src/components/MenuView.tsx
+++ src/components/MenuView.tsx
@@ -7,15 +7,6 @@ interface IMenuEditorProps {
     menu: IMenuData;
 };
 
-const traverse = (menu: any, depth: number, selected: number[]): IMenuData[] => {
-    const childSelectedIndex = selected[depth];
-    if(depth < selected.length && menu.hasOwnProperty('children') && menu.children.length > 0) {
-        const selectedChild = menu.children[childSelectedIndex];
-        return [selectedChild, ...traverse(selectedChild, depth+1, selected)];
-    }
-    return [];
-};
-
 export default class MenuEditor extends React.Component<IMenuEditorProps, any> {
     constructor(props: IMenuEditorProps){
         super(props);
@@ -24,7 +15,18 @@ export default class MenuEditor extends React.Component<IMenuEditorProps, any> {
         };
         this.onSelectHandler = this.onSelectHandler.bind(this);
         this.renderAllMenus = this.renderAllMenus.bind(this);
+        this.traverse = this.traverse.bind(this);
     }
+
+    traverse (menu: any, depth: number = 0): IMenuData[] {
+        const {selected} = this.state;
+        const childSelectedIndex = selected[depth];
+        if(depth < selected.length && menu.hasOwnProperty('children') && menu.children.length > 0) {
+            const selectedChild = menu.children[childSelectedIndex];
+            return [selectedChild, ...this.traverse(selectedChild, depth+1)];
+        }
+        return [];
+    };
     onSelectHandler(index: number, depth: number = 0) {
         const newSelected = _.slice(this.state.selected, 0, depth + 1);
         newSelected[depth] = index;
@@ -32,7 +34,7 @@ export default class MenuEditor extends React.Component<IMenuEditorProps, any> {
         this.setState({selected:newSelected});
     }
     renderAllMenus(menu: IMenuData) {
-        const menuItems = traverse(menu, 0, this.state.selected);
+        const menuItems = this.traverse(menu);
         return menuItems.map((menuItem, depth) =>
             <MenuBasic menu={menuItem}
                        onSelectHandler={(selectedIndex) => this.onSelectHandler(selectedIndex, depth + 1)}/>
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import {IMenuData} from "./components/Menu";
-import MenuEditor from "./components/MenuEditor1";
+import MenuEditor from "./components/MenuView";
 
 const menu : IMenuData = {
     label: 'Blah',
